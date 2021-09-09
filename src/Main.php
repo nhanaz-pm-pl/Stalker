f8192031c9ec8c1e087539cbc61b1c2f7d973dcf
Nguyễn Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2021-09-09 14:55:29 +0700

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -4,24 +4,43 @@ declare(strict_types=1);
 
 namespace NhanAZ\Track;
 
-use pocketmine\Server;
-use pocketmine\utils\Config;
-use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\server\CommandEvent;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\{
+    Server,
+    utils\Config,
+    event\Listener,
+    utils\TextFormat,
+    plugin\PluginBase,
+    event\server\CommandEvent,
+    event\player\PlayerCommandPreprocessEvent};
 use NhanAZ\Track\libs\JackMD\UpdateNotifier\UpdateNotifier;
 
 class Main extends PluginBase implements Listener
 {
 
-    public CONST InvalidConfig = 'Invalid config. Please check config.yml again. Thank you.';
-    
+    public CONST InvalidConfig = TextFormat::DARK_RED . "NoticeRemoved in config.yml doesn't exist";
+    public CONST HandleFont = TextFormat::ESCAPE . "　";
+
     public $history;
 
     public function onLoad() : void 
     {
-        UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
+        $description = $this->getDescription()->getName();
+        $version =  $this->getDescription()->getVersion();
+        UpdateNotifier::checkUpdate($description, $version);
+    }
+
+    public function InvalidConfig() : void
+    {
+        $this->history->save();
+        $NoticeRemoved = $this->getConfig()->get('NoticeRemoved', self::InvalidConfig);
+        $this->getLogger()->info($NoticeRemoved);
+    }
+
+    public function RemoveConfig() : void
+    {
+        foreach ($this->history->getAll() as $history => $data) {
+            $this->history->remove($history);
+        }
     }
 
     public function onEnable() : void
@@ -30,26 +49,17 @@ class Main extends PluginBase implements Listener
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveResource('history.yml');
         $this->history = new Config($this->getDataFolder().'history.yml', Config::YAML);
-
         if ($this->getConfig()->get('DeleteHistory')['onEnable'] == true) {
-            foreach ($this->history->getAll() as $history => $data) {
-                $this->history->remove($history);
-            }
-            $this->history->save();
-            $NoticeRemoved = $this->getConfig()->get('NoticeRemoved', self::InvalidConfig);
-            $this->getLogger()->info($NoticeRemoved);
+            $this->RemoveConfig();
+            $this->InvalidConfig();
         } 
     }
 
     public function onDisable() : void
     {
         if ($this->getConfig()->get('DeleteHistory')['onDisable'] == true) {
-            foreach ($this->history->getAll() as $history => $data) {
-                $this->history->remove($history);
-            }
-            $this->history->save();
-            $NoticeRemoved = $this->getConfig()->get('NoticeRemoved', self::InvalidConfig);
-            $this->getLogger()->info($NoticeRemoved);
+            $this->RemoveConfig();
+            $this->InvalidConfig();
         }
     }
 
@@ -60,15 +70,15 @@ class Main extends PluginBase implements Listener
         $name = $event->getSender()->getName();
         $this->history->set("{$time} : {$name}", $cmd);
         $this->history->save();
-        $this->getLogger()->info("{$name} > {$cmd}");
+        $this->getLogger()->info("{$name} > /{$cmd}");
         $trackers = $this->getConfig()->get('Trackers');
         foreach ($trackers as $tracker) {
             $tracker = $this->getServer()->getPlayerByPrefix($tracker);
             if ($tracker) {
                 (string) $prefix = $this->getDescription()->getPrefix();
                 $UnicodeFont = $this->getConfig()->get('UnicodeFont');
-                $Handle_Variable_UnicodeFont = ($UnicodeFont == true ? self::Handle_Font : '');
-                $tracker->sendMessage("{$Handle_Variable_UnicodeFont} [{$prefix}] {$name} > {$cmd}");
+                $Handle_Variable_UnicodeFont = ($UnicodeFont == true ? self::HandleFont : '');
+                $tracker->sendMessage("[{$prefix}] {$name} > /{$cmd}{$Handle_Variable_UnicodeFont}");
             }
         }
         return true;

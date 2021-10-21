acaa3be4035742a7b82ba65d5557a3d9f425efbc
Thành Nhân <nhangaming125@gmail.com>
2021-10-21 08:05:36 +0700

    Update Main.php

--- src/NhanAZ/Track/Main.php
+++ src/NhanAZ/Track/Main.php
@@ -7,78 +7,70 @@ namespace NhanAZ\Track;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\server\CommandEvent;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use NhanAZ\Track\libs\JackMD\UpdateNotifier\UpdateNotifier;
 
 class Main extends PluginBase implements Listener
 {
 
-    public CONST InvalidConfig = TextFormat::DARK_RED . "NoticeRemoved in config.yml doesn't exist";
-    public CONST HandleFont = TextFormat::ESCAPE . "　";
+	public CONST InvalidConfig = 'Invalid config. Please check config.yml again. Thank you.';
 
-    public $history;
+	public $history;
 
-    public function onLoad() : void
-    {
-        $description = $this->getDescription()->getName();
-        $version =  $this->getDescription()->getVersion();
-        UpdateNotifier::checkUpdate($description, $version);
-    }
+	public function onLoad() : void
+	{
+		UpdateNotifier::checkUpdate($this->getDescription()->getName(), $this->getDescription()->getVersion());
+	}
 
-    public function InvalidConfig() : void
-    {
-        $this->history->save();
-        $NoticeRemoved = $this->getConfig()->get("NoticeRemoved", self::InvalidConfig);
-        $this->getLogger()->info($NoticeRemoved);
-    }
+	public function onEnable() : void
+	{
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveResource('history.yml');
+		$this->history = new Config($this->getDataFolder().'history.yml', Config::YAML);
 
-    public function RemoveConfig() : void
-    {
-        foreach ($this->history->getAll() as $history => $data) {
-            $this->history->remove($history);
-        }
-    }
+		if ($this->getConfig()->get('DeleteHistory')['onEnable'] == true) {
+			foreach ($this->history->getAll() as $history => $data) {
+				$this->history->remove($history);
+			}
+			$this->history->save();
+			$NoticeRemoved = $this->getConfig()->get('NoticeRemoved', self::InvalidConfig);
+			$this->getLogger()->info($NoticeRemoved);
+		}
+	}
 
-    public function onEnable() : void
-    {
-        $this->saveDefaultConfig();
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->saveResource("history.yml");
-        $this->history = new Config($this->getDataFolder()."history.yml", Config::YAML);
-        if ($this->getConfig()->get("DeleteHistory")["onEnable"] == true) {
-            $this->RemoveConfig();
-            $this->InvalidConfig();
-        }
-    }
+	public function onDisable() : void
+	{
+		if ($this->getConfig()->get('DeleteHistory')['onDisable'] == true) {
+			foreach ($this->history->getAll() as $history => $data) {
+				$this->history->remove($history);
+			}
+			$this->history->save();
+			$NoticeRemoved = $this->getConfig()->get('NoticeRemoved', self::InvalidConfig);
+			$this->getLogger()->info($NoticeRemoved);
+		}
+	}
 
-    public function onDisable() : void
-    {
-        if ($this->getConfig()->get("DeleteHistory")["onDisable"] == true) {
-            $this->RemoveConfig();
-            $this->InvalidConfig();
-        }
-    }
-
-    public function onCommandEvent(CommandEvent $event)
-    {
-        $cmd = $event->getCommand();
-        $time = date("D d/m/Y H:i:s(A)");
-        $name = $event->getSender()->getName();
-        $this->history->set("{$time} : {$name}", $cmd);
-        $this->history->save();
-        $this->getLogger()->info("{$name} > /{$cmd}");
-        $trackers = $this->getConfig()->get("Trackers");
-        foreach ($trackers as $tracker) {
-            $tracker = $this->getServer()->getPlayerByPrefix($tracker);
-            if ($tracker) {
-                (string) $prefix = $this->getDescription()->getPrefix();
-                $UnicodeFont = $this->getConfig()->get("UnicodeFont");
-                $Handle_Variable_UnicodeFont = ($UnicodeFont == true ? self::HandleFont : "");
-                $tracker->sendMessage("{$Handle_Variable_UnicodeFont}[{$prefix}] {$name} > /{$cmd}");
-            }
-        }
-        return true;
-    }
+	public function onCommandEvent(CommandEvent $event)
+	{
+		$cmd = $event->getCommand();
+		$time = date('D d/m/Y H:i:s(A)');
+		$name = $event->getSender()->getName();
+		$this->history->set("{$time} : {$name}", $cmd);
+		$this->history->save();
+		$this->getLogger()->info("{$name} > {$cmd}");
+		$trackers = $this->getConfig()->get('Trackers');
+		foreach ($trackers as $tracker) {
+			$tracker = $this->getServer()->getPlayerByPrefix($tracker);
+			if ($tracker) {
+				(string) $prefix = $this->getDescription()->getPrefix();
+				$UnicodeFont = $this->getConfig()->get('UnicodeFont');
+				$Handle_Variable_UnicodeFont = ($UnicodeFont == true ? self::Handle_Font : '');
+				$tracker->sendMessage("{$Handle_Variable_UnicodeFont} [{$prefix}] {$name} > {$cmd}");
+			}
+		}
+		return true;
+	}
 }

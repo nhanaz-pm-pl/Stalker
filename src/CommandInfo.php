05e7e208e4f6920abb3384ac38b8f48b2a401120
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-07 01:22:23 +0800

    Added CommandInfo.php

---
+++ src/CommandInfo.php
@@ -0,0 +1,21 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace NhanAZ\Track;
+
+use SOFe\InfoAPI\Info;
+
+class CommandInfo extends Info
+{
+
+    public function __construct()
+    {
+    }
+
+    public function toString() : string
+    {
+        // TODO: Implement toString() method.
+    }
+}

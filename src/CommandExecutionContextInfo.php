d8c739c122f7a19fccc31a76d525b9438d872e9f
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-07 02:32:52 +0800

    Added CommandExecutionContextInfo.php

---
+++ src/CommandExecutionContextInfo.php
@@ -0,0 +1,25 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace NhanAZ\Track;
+
+use SOFe\InfoAPI\Info;
+
+class CommandExecutionContextInfo extends Info
+{
+
+    public function __construct()
+    {
+    }
+
+    public static function init() : void {
+
+    }
+
+    public function toString() : string
+    {
+        // TODO: Implement toString() method.
+    }
+}

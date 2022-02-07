99f2a6d8485de38c5e7268079f778f76758c11ae
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-07 17:16:03 +0800

    Added UtilsInfo.php

---
+++ src/utils/UtilsInfo.php
@@ -0,0 +1,20 @@
+<?php
+
+
+declare(strict_types=1);
+
+use SOFe\InfoAPI\Info;
+
+class UtilsInfo extends Info
+{
+
+    public static function init() : void
+    {
+
+    }
+
+    public function toString() : string
+    {
+        // TODO: Implement toString() method.
+    }
+}

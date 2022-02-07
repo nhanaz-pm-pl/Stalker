0dfc2a6ffb1b2abfae1c2465c95cbfd5f5b2eebd
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2022-02-07 17:18:46 +0800

    Provide info Track.Utils.UnicodeFont

--- src/utils/UtilsInfo.php
+++ src/utils/UtilsInfo.php
@@ -4,13 +4,22 @@
 declare(strict_types=1);
 
 use SOFe\InfoAPI\Info;
+use SOFe\InfoAPI\InfoAPI;
+use SOFe\InfoAPI\StringInfo;
 
 class UtilsInfo extends Info
 {
 
     public static function init() : void
     {
-
+        InfoAPI::provideInfo(
+            self::class,
+            StringInfo::class,
+            "Track.Utils.UnicodeFont",
+            fn(self $info) : StringInfo => new StringInfo(
+                \NhanAZ\Track\Main::HandleFont
+            )
+        );
     }
 
     public function toString() : string

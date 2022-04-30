8d4e38ad93adc47d0378b55c2218b7d3965478c6
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-04-30 11:36:36 +0700

    getNested instead of ()[]

--- src/Main.php
+++ src/Main.php
@@ -44,7 +44,7 @@ class Main extends PluginBase implements Listener {
 		$this->saveDefaultConfig();
 		$this->saveResource("history.yml");
 		$this->history = new Config($this->getDataFolder() . "history.yml", Config::YAML);
-		if ($this->getConfig()->get("DeleteHistory")["onEnable"]) {
+		if ($this->getConfig()->getNested("DeleteHistory.onEnable")) {
 			$this->RemoveConfig();
 			$this->InvalidConfig();
 		}
@@ -57,7 +57,7 @@ class Main extends PluginBase implements Listener {
 	}
 
 	public function onDisable(): void {
-		if ($this->getConfig()->get("DeleteHistory")["onDisable"]) {
+		if ($this->getConfig()->getNested("DeleteHistory.onDisable")) {
 			$this->RemoveConfig();
 			$this->InvalidConfig();
 		}

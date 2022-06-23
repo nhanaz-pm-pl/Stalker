ab8bbc8f3b2ab84cb36a0850ad4ed5a2fa1b7106
NhanAZ <60387689+NhanAZ@users.noreply.github.com>
2022-06-23 19:14:51 +0700

    .log instead .yaml

--- src/Main.php
+++ src/Main.php
@@ -4,45 +4,63 @@ declare(strict_types=1);
 
 namespace NhanAZ\Track;
 
-use pocketmine\utils\Config;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\server\CommandEvent;
 
 class Main extends PluginBase implements Listener {
 
-	public $log;
-
-	public function RemoveConfig(): void {
-		foreach ($this->log->getAll() as $log => $data) {
-			$this->log->remove($log);
-		}
-	}
+	private string $logPath;
 
 	protected function onEnable(): void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->initConfig();
+		$this->initLog();
+		$this->deleteLog();
+	}
+
+	private function initLog(): void {
+		$this->logPath = $this->getDataFolder() . "log.log";
+		if (!file_exists($this->logPath)) $this->createLogFile();
+	}
+
+	private function createLogFile() {
+		file_put_contents($this->logPath, "");
+	}
+
+	private function initConfig(): void {
 		$this->saveDefaultConfig();
-		$this->saveResource("log.yml");
-		$this->log = new Config($this->getDataFolder() . "log.yml", Config::YAML);
-		if ($this->getConfig()->getNested("deleteLog.onEnable")) {
-			$this->RemoveConfig();
+	}
+
+	private function deleteLog(): void {
+		if ($this->getConfig()->getNested("deleteLog.onEnable", false)) {
+			$this->createLogFile();
+		}
+		if ($this->getConfig()->getNested("deleteLog.onDisable", false)) {
+			$this->createLogFile();
 		}
 	}
 
 	protected function onDisable(): void {
-		if ($this->getConfig()->getNested("deleteLog.onDisable")) {
-			$this->RemoveConfig();
+		$this->deleteLog();
+	}
+
+	private function onLog($time, $sender, $command): void {
+		if (filesize($this->logPath) / 1048576 >= $this->getConfig()->get("MaxSize", 16)) {
+			$this->createLogFile();
 		}
+		$message = $time . " [" . $sender . "]: /" . $command . PHP_EOL;
+		file_put_contents($this->logPath, $message, FILE_APPEND);
+		clearstatcache(true, $this->logPath);
 	}
 
 	public function onCommandEvent(CommandEvent $event) {
 		$cmd = $event->getCommand();
 
-		$time = date("D d/m/Y H:i:s(A)");
+		$time = date("Y-m-d [H:i:s]");
 		$name = $event->getSender()->getName();
 
-		$this->log->set("{$time} : {$name}", $cmd);
-		$this->log->save();
+		$this->onLog($time, $name, $cmd);
 
 		$this->getLogger()->info("{$name} > /{$cmd}");
 
